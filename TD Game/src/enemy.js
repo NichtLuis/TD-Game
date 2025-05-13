c6e6317671e22a12d21402aa97a88c5b5373bcf4
mirko4001 <hyyazan234@gmail.com>
2025-05-13 08:34:53 +0200

    fixed duplicate code in enemy and Tutorial js-files

--- TD Game/src/enemy.js
+++ TD Game/src/enemy.js
@@ -1,57 +1,38 @@
-
 export class Enemy {
-    constructor(x, y, speed, money, health, damage, flying, width, height, image) {
-        this.x = x;
-        this.y = y;
-        this.health = health; // Health of the enemy
-        this.speed = speed;
-        this.damage = damage; // Damage dealt by the enemy   
-        this.money = money;
-        this.flying = flying;
-        this.width = width;
-        this.height = height;
+  constructor(
+    x,
+    y,
+    speed,
+    money,
+    health,
+    damage,
+    flying,
+    width,
+    height,
+    image
+  ) {
+    this.x = x;
+    this.y = y;
+    this.health = health; // Health of the enemy
+    this.speed = speed;
+    this.damage = damage; // Damage dealt by the enemy
+    this.money = money;
+    this.flying = flying;
+    this.width = width;
+    this.height = height;
 
-        if (image) {
-            this.image = new Image();
-            this.image.src = image;
-        } else {
-            this.image = null; // If no image is provided, set it to null
-        }
+    if (image) {
+      this.image = new Image();
+      this.image.src = image;
+    } else {
+      this.image = null; // If no image is provided, set it to null
     }
+  }
 
-    //draw(ctx) {
-        //ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    //}
+  //draw(ctx) {
+  //ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  //}
 
-    //move() {
-       // this.x -= this.speed; // Move left
-      // }
-
-class Enemy {
-    constructor(x, y, speed, money, health, damage, flying, width, height, image) {
-        this.x = x;
-        this.y = y;
-        this.speed = speed;
-        this.money = money;
-        this.health = health; // Health of the enemy
-        this.damage = damage; // Damage dealt by the enemy   
-        this.flying = flying;
-        this.width = width;
-        this.height = height;
-
-        if (image) {
-            this.image = new Image();
-            this.image.src = image;
-        } else {
-            this.image = null; // If no image is provided, set it to null
-        }
-    }
-
-    //draw(ctx) {
-        //ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    //}
-
-    //move() {
-       // this.x -= this.speed; // Move left
-      // }
-    }
+  //move() {
+  // this.x -= this.speed; // Move left
+}
